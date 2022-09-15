d37d9c013cb2241bc4523ce46a6d503d140ad188
ucsus <ucsusman@gmail.com>
2022-09-15 13:23:54 +0300

    use random

--- src/test/java/ru/testmepls/ExampleTest.java
+++ src/test/java/ru/testmepls/ExampleTest.java
@@ -3,14 +3,17 @@ package ru.testmepls;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExampleTest {
 
     @Test
     void exampleTest0() {
-        Assertions.assertTrue(2 > 1);
+        Assertions.assertTrue(new Random().nextInt() > 1);
     }
 
     @Test
-    void exampleTest1() { Assertions.assertTrue(3 > 2);
+    void exampleTest1() {
+        Assertions.assertTrue(new Random().nextInt() > 2);
     }
 }
